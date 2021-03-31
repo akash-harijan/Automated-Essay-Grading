087f4fd3f913a000bc21415aa85c01d4c9e8b79a
Akash Harijan <akashdharani7@gmail.com>
2021-03-31 12:25:32 +0500

    update dataloder class

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,19 +10,36 @@ class DataLoader:
         self.data_path = data_path
         self.emb_path = emb_path
 
-    self.word2vec_dict = {}
-    with open(self.emb_path) as f:
-        for line in f:
-            l = line.split()
-            word = l[0]
-            word_emb = np.array(l[1:], dtype=np.float64)
-            self.word2vec_dict[word] = word_emb
+        self.whole_data = pd.read_csv(self.data_path, sep='\t', encoding='unicode_escape')
+        self.whole_data = self.whole_data.fillna(0)
+        self.total_essay_sets = self.whole_data.essay_set.unique()
 
-    def text_2_num(self, essay_id):
+        print("DataSet is loaded with {0} Shape".format(self.whole_data.shape))
+
+        self.word2vec_dict = {}
+        with open(self.emb_path) as f:
+            for line in f:
+                l = line.split()
+                word = l[0]
+                word_emb = np.array(l[1:], dtype=np.float64)
+                self.word2vec_dict[word] = word_emb
+
+    def get_essay_data(self, essay_id):
+        essay_ds = self.whole_data[self.whole_data.essay_set == essay_id]
+        essay_ds = np.array(essay_ds)
+        print("Dataset with Essay id {0} has {1} Shape".format(essay_id, essay_ds.shape))
+
+        essay_text = np.array(essay_ds[:, 2])
+        essay_score = np.array(essay_ds[:, 5])
+
+        train_y = essay_score.reshape(essay_score.shape[0], 1)
+
+        print("Test X ", essay_text[0])
+        print("Test Y ", essay_score[0])
 
         train_x = []
-        for i in range(trainx.shape[0]):
-            sentence = trainx[i]
+        for i in range(essay_text.shape[0]):
+            sentence = essay_text[i]
             emb = np.zeros(300)
             words = sentence.split()
             t_n = 0
@@ -34,20 +51,35 @@ class DataLoader:
                 except:
                     skip += 1
                     pass
-
             train_x.append(emb / t_n)
-        return train_x
 
+        return np.array(train_x), np.array(train_y)
+
+    def get_data(self):
+        train_X, train_Y, val_X, val_Y = [], [], [], []
+        for i in self.total_essay_sets:
+            x, y = self.get_essay_data(i)
+            split = int(x.shape[0]*0.8)
+            train_X.append(x[:split])
+            train_Y.append(y[:split])
+            val_X.append(x[split:])
+            val_Y.append(y[split:])
+            print(x.shape, y.shape)
+
+        train_X = np.vstack(train_X)
+        train_Y = np.vstack(train_Y)
+
+        val_X = np.vstack(val_X)
+        val_Y = np.vstack(val_Y)
+
+        print("train X Shape {0} and Y Shape {1}, validation X shape {2} and Y shape {3}".format(train_X.shape, train_Y.shape, val_X.shape, val_Y.shape))
+
+        return train_X, train_Y, val_X, val_Y
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    dataset_path = "/home/akash/Pet/Auto Essay Grading-old/Auto Essay Grading/Akash/Data/training_set_rel3.tsv"
+    emb_path = "/home/akash/Pet/Auto Essay Grading-old/Auto Essay Grading/automated-essay-grading/glove/glove.42B.300d.txt"
+    dataset = DataLoader(dataset_path, emb_path)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    dataset.get_data()
